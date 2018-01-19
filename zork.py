090b69e0ccf02276e19b18b4d925b5935ae33ccf
Jason Wong <jwong1@ucvts.tecnj.us>
2018-01-18 22:56:46 -0500

    Created game file

---
+++ zork.py
@@ -0,0 +1,8 @@
+
+def intro():
+    inventory = []
+    print("--------------------------------------------------")
+    one = input("West of House\nYou are standing in an open field west of a white house, with a boarded front door.\nThere is a small mailbox here.\n>")
+    if one == "open mailbox":
+        print("Open mailbox")
+        
